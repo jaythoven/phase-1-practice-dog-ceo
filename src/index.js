0cc99e58908cc1d1815adc73c6e55ce4f743b304
Jonathan Aguilar <j.aguilar0390@gmail.com>
2022-05-04 21:33:19 -0400

    commit tree b535b1ab1a8763c79985a2ded5114eee65ec814b

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+
+const dogFetch = () => {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+    .then(response => response.json)
+    .then(data => console.log(data))
+}
